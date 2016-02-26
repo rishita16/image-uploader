287de7f69e6d27d39888b896378f290fbb1bb52b
rishita16 <rishita.mehal@gmail.com>
2016-02-26 16:58:47 +0530

    removed unused files

--- app/components/Dropzone.js
+++ app/components/Dropzone.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import DropzoneComponent from 'react-dropzone-component';
 
-
-;
 const componentConfig = {
     iconFiletypes: ['.jpg', '.png', '.gif'],
     showFiletypeIcon: true,
@@ -13,68 +11,17 @@ const djsConfig = {
     maxFiles:10,
     acceptedFiles: 'image/jpeg,image/png,image/gif',
     uploadMultiple:true,
-    autoProcessQueue:false,
     addRemoveLinks:true,
     dictDefaultMessage: 'Drop File(s) Here or Click to Upload Images'
 };
 
-var simpleCallBack = function () {
-    console.log('I\'m a simple callback');
-};
-
-var remove = function() {
-  alert("file was removed successfully!")
-}
-
-
-const eventHandlers = {
-    // All of these receive the event as first parameter:
-    drop:simpleCallBack,
-    dragstart: null,
-    dragend: null,
-    dragenter: null,
-    dragover: null,
-    dragleave: null,
-    // All of these receive the file as first parameter:
-    addedfile: '',
-    removedfile:null,
-    thumbnail: null,
-    error: null,
-    processing: null,
-    uploadprogress: null,
-    sending: null,
-    success: null,
-    complete: null,
-    canceled: null,
-    maxfilesreached: null,
-    maxfilesexceeded: null,
-    // All of these receive a list of files as first parameter
-    // and are only called if the uploadMultiple option
-    // in djsConfig is true:
-    processingmultiple: null,
-    sendingmultiple: null,
-    successmultiple: null,
-    completemultiple: null,
-    canceledmultiple: null,
-    // Special Events
-    totaluploadprogress: null,
-    reset: null,
-    queuecompleted: null
-}
-
 export default class DragDrop extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      files: []
-    }
-  }
   render() {
     return (
-        <div>
-          <DropzoneComponent config={componentConfig} djsConfig={djsConfig} eventHandlers={eventHandlers} />
-        </div>
-        )
-}
+      <div>
+        <DropzoneComponent config={componentConfig} djsConfig={djsConfig} />
+      </div>
+    )
+  }
 }
