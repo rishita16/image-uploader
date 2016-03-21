01b377846f8afd172412c879e9d589b730cfa704
rishita16 <rishita.mehal@gmail.com>
2016-03-21 17:38:10 +0530

    refactored code

--- server.js
+++ server.js
@@ -1,61 +1,58 @@
-/* eslint no-console: 0 */
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
-const app = express();
-const multer  = require('multer');
-const upload = multer();
-const uploads = require('./app/controller/aws.js')
+  /* eslint no-console: 0 */
+  const path = require('path');
+  const express = require('express');
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.config.js');
+  const isDeveloping = process.env.NODE_ENV !== 'production';
+  const port = isDeveloping ? 3000 : process.env.PORT;
+  const app = express();
+  const multer  = require('multer');
+  const upload = multer();
+  const ImageUpload = require('./app/controller/ImageUpload.js')
 
-if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
+  if (isDeveloping) {
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+      contentBase: 'src',
+      stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+      }
+    });
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+
+    app.get('/', function ( req , res) {
+      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.tpl.html')));
+      res.end();
+    });
+  } 
+
+  else {
+    app.use(express.static(__dirname + '/dist'));
+    app.get('/', function(err,req, res) {
+      res.sendFile(path.join(__dirname, 'dist/index.tpl.html'));
+    });
+  }
+
+  app.post('/upload', upload.single('file[0]'), function (req , res , next) {
+    res.send(req.file);
+    ImageUpload(req,res);
   });
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
 
-
-  app.get('/', function ( req , res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.tpl.html')));
-    res.end();
+  app.listen(port, '0.0.0.0', function (err) {if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
   });
-} 
-
-else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('/', function(err,req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.tpl.html'));
-
-  });
-}
-
-app.post('/upload', upload.array('file[0]'), function (req , res , next) {
-  res.send(req.files);
-  uploads(req,res);
- 
-});
-
-app.listen(port, '0.0.0.0', function (err) {if (err) {
-  console.log(err);
-}
-console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-});
-
 
 
